d2ae2da7c12ece3a629d8ab712f551caa9fb632d
Alfredocgn <alfredocgn@gmail.com>
2023-12-04 23:17:04 -0300

    Card slide

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import video from '../assets/video.mp4'
 import {IoPlayCircleSharp} from "react-icons/io5"
@@ -16,35 +17,35 @@ export const Card = ({movieData,isLiked =false}) => {
             {
                 isHovered && (
                     <div className='hover z-40 h-max w-[20rem] absolute top-[-18vh] left-0  rounded-md bg-[#181818] transition-all ease-in-out duration-300 '>
-                        <div className='image-container relative h-[140px]'>
+                        <div className='image-container relative h-[140px] w-full'>
                             <img className='w-[100%] h-[140px] bg-cover rounded-md top-0 z-30 absolute' src={`https://image.tmdb.org/t/p/w500${movieData.image}`} alt="movie " onClick={() => navigate("/player")}/>
-                            <video className='w-[100%] h-[140px] rounded-md top-0 z-50 absolute' src={video} autoPlay loop muted onClick={() => navigate("/player")} />
+                            <video className='w-full h-[140px] rounded-md top-0 z-50 absolute' src={video} autoPlay loop muted onClick={() => navigate("/player")} />
 
                         </div>
-                        <div className="info-container flex flex-col">
+                        <div className="info-container flex flex-col p-4 gap-2">
                             <h3 className='name' onClick={()=> navigate("/player")}>{movieData.name}</h3>
-                            <div className='icons flex justify-between'>
-                                <div className='controls flex'>
-                                    <IoPlayCircleSharp title="play" onClick={()=> navigate("/player")}/>
-                                    <RiThumbUpFill title="Like"/>
-                                    <RiThumbDownFill title="Dislike"/>
+                            <div className='icons flex justify-between '>
+                                <div className='controls flex gap-4'>
+                                    <IoPlayCircleSharp className='text-xl cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title="play" onClick={()=> navigate("/player")}/>
+                                    <RiThumbUpFill className='text-md cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title="Like"/>
+                                    <RiThumbDownFill className='text-xl cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title="Dislike"/>
                                     {
                                         isLiked ? (
-                                            <BsCheck title="Remove from list"/> ) : (
-                                                <AiOutlinePlus title="Add to list"/>
+                                            <BsCheck className='text-xl cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title="Remove from list"/> ) : (
+                                            <AiOutlinePlus className='text-xl cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title="Add to list"/>
                                         )
                                     }
                                 </div>
-                                <div className='info'>
-                                    <BiChevronDown title='More info'/>
+                                <div className='info  '>
+                                    <BiChevronDown className='text-xl cursor-pointer transition-all duration-300 ease-in-out hover:text-[#b8b8b8]' title='More info'/>
 
                                 </div>
                             </div>
                             <div className='genres flex'>
-                                <ul className='flex'>
+                                <ul className='flex gap-4 '>
                                     {
                                         movieData.genres.map((genre)=>{
-                                            return <li key={genre}>{genre}</li>
+                                            return <li className='pr-3' key={genre}>{genre}</li>
                                         })
                                     }
                                 </ul>
--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,21 +1,52 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useRef, useState } from "react";
 import { Card } from "./Card"
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 
 export const CardSlider = ({data,title}) => {
-    console.log(data)
+    const [showControls,setShowControls]=useState(false)
+    const[sliderPosition,setSliderPosition]=useState(0)
+    const listRef = useRef()
+
     if (!data) {
         return <div>Loading...</div>;
     }
 
     const movieResults = data || []
     
+    const handleDirection = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 70;
+        if(direction === "left" && sliderPosition > 0){
+            listRef.current.className +=("translate-x-230")
+            setSliderPosition(sliderPosition - 1)
+        }
+        if(direction === "right" && sliderPosition < 4){
+            listRef.current.className +=("translate-x--230")
+            setSliderPosition(sliderPosition + 1)
+        }
+    }
+    console.log(listRef)
     return (
-        <div className="flex ">
-            {
-                movieResults.map((movie) => {
-                    return <Card movieData={movie} key={movie.id} />
-                })
-            }
+        
+        <div className="Container flex flex-col gap-4 relative py-8 px-0 " onMouseEnter={()=> setShowControls(true)} onMouseLeave={()=> setShowControls(false)}>
+            <h1 className="ml-[15px]">{title}</h1>
+            <div className="wrapper">
+                    <div className={`slider-action cursor-pointer left-0 left  ${showControls ? "none" : ""} flex items-center justify-center absolute z-100 h-[100%] top-5 bottom-0 w-[50px] transition-all duration-300 ease-in-out `}>
+                        <AiOutlineLeft className="text-xl" onClick={()=> handleDirection("left")}/>
+                    </div>
+                <div className="flex slider w-max gap-4 translate-x-0 transition-all duration-300 ease-in-out ml-[50px] " ref={listRef}>
+                {
+                    movieResults.map((movie) => {
+                        return <Card movieData={movie} key={movie.id} />
+                    })
+                }
+                </div>
+                <div className={`slider-action right cursor-pointer right-0 ${showControls ? "none" : ""} flex items-center justify-center absolute z-100 h-[100%] top-5 bottom-0 w-[50px] transition-all duration-300 ease-in-out `}>
+                        <AiOutlineRight className="text-xl" onClick={()=> handleDirection("right")}/>
+                    </div>
+            </div>
         </div>
     )
 }
